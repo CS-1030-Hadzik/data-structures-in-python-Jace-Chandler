6f85ef82308d2435fe3585442e9e99e524d9c788
Jace-Chandler <jacechandler@mail.weber.edu>
2023-11-27 18:36:19 -0700

    Completed Problems

--- array.py
+++ array.py
@@ -11,29 +11,43 @@ print(car)
 print(len(car))
 
 # `problem 5. Append Buick to the Array`
+car.append('Buick')
 
 # `problem 6. print the array to the console`
+print(car)
 
 # `problem 7. Print the 4th element in the array (Not index 4, element 4)`
+print(car[3])
 
 # `problem 8. Insert 'Toyota' into element 3 in the array`
+car.insert(2,'Toyota')
 
 # `problem 9. print the array to the console`
+print(car)
 
 # `problem 10. Remove element 5 of the array (hint look at options for pop())`
+car.pop(4)
 
 # `problem 11. print the array to the console`
+print(car)
 
 # `problem 12. Sort the array in ascending order`
+car.sort()
 
 # `problem 13. print the array to the console`
+print(car)
 
 # `problem 14. Sort the array in descending order`
+car.sort(reverse = True)
 
 # `problem 15. print the array to the console`
+print(car)
 
 # `problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
+my_array_length =len(car)
 
 # `problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
+array_string = 'The length of my array is '
 
 # `problem 18. print array_string concatenated with my_array_length to the console.`
+print(array_string,my_array_length)
